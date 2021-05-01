e803bf8e136337c93ca5b7ddfc1264209f91a7a4
Hasibul-islam-it <hasibulislam5088872@gmail.com>
2021-05-01 12:42:04 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+// let pakhi;
+ function add(num1,num2){
+     console.log(num1 + num2);
+     return
+}
+
+ function add(num1,num2){
+ console.log(num1,num2);
+ }
+
+//Trying to access something that is not here:-
+const lover = { name:"smart boy omuk", phone:01700000};
+console.log(lover.gf);
+
+// The undidined thing is negative or false:-
+let funny = undefined;
+console.log(funny);
+
